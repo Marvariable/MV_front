93c882dfc448bb0c1706e011822550379edecacd
Cristina Viejó <krissvinti@gmail.com>
2026-03-23 11:01:41 +0100

    MAR Rutas añadidas de App.jsx

--- marvariable/src/App.jsx
+++ marvariable/src/App.jsx
@@ -1,37 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Publications from "./pages/Publications"
-import Theory from "./pages/Theory"
-import Narrative from "./pages/Narrative"
+import { Routes, Route } from "react-router-dom";
+
+import PublicLayout from "./components/layout/PublicLayout";
+import AdminLayout from "./components/admin/AdminLayout";
+
+import Home from "./pages/Home";
+import Publications from "./pages/Publications";
+import Theory from "./pages/Theory";
+import Narrative from "./pages/Narrative";
 import Theater from "./pages/Theater";
-import Rhumor from "./pages/Rhumor"
-import AboutAuthor from "./pages/AboutAuthor"
-import Contact from "./pages/Contact"
-import AdminRegister from "./pages/AdminRegister"
+import Rhumor from "./pages/Rhumor";
+import AboutAuthor from "./pages/AboutAuthor";
+import Contact from "./pages/Contact";
+import AdminRegister from "./pages/AdminRegister";
 
-
-
+import PublicationsList from "./pages/admin/PublicationsList";
+import CreatePublication from "./pages/admin/CreatePublication";
+import EditPublication from "./pages/admin/EditPublication";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-
-      <Routes>
+    <Routes>
+      {/* RUTAS PÚBLICAS */}
+      <Route element={<PublicLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/publications" element={<Publications />} />
         <Route path="/theory" element={<Theory />} />
         <Route path="/narrative" element={<Narrative />} />
-        <Route path="/theater" element= {<Theater/>} />
+        <Route path="/theater" element={<Theater />} />
         <Route path="/rhumor" element={<Rhumor />} />
         <Route path="/about-author" element={<AboutAuthor />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/admin-register" element={<AdminRegister />} />
-        
-      </Routes>
-    </div>
-  )
+      </Route>
+
+     
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="publicaciones" element={<PublicationsList />} />
+        <Route path="publicaciones/nueva" element={<CreatePublication />} />
+        <Route
+          path="publicaciones/:id/editar"
+          element={<EditPublication />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
